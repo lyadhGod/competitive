fb78757e29ad858f984b6c6c0db2b2159d9f72e9
Rounak Tikadar <rounak.tikadar@gmail.com>
2021-05-15 12:49:37 +0530

    perfected string splitter algo

--- Algorithms/Strings/split.cpp
+++ Algorithms/Strings/split.cpp
@@ -3,29 +3,78 @@
 using namespace std;
 
 vector<string> split(const string& s, const string& d) {
-  vector<string> v;
+  size_t start, end;
 
-  size_t start = 0;
-  size_t end = s.find_first_of(d);
+  int count = 0;
+  start = 0;
+  end = s.find_first_of(d);
   while (end != string::npos) {
-    v.emplace_back(s.substr(start, end - start));
+    count++;
     start = end + 1;
     end = s.find_first_of(d, start);
   }
-  v.emplace_back(s.substr(start, s.size() - start));
+
+  vector<string> v;
+  int i;
+  if (count > 0) {
+    v.resize(count + 1);
+
+    i = 0;
+    start = 0;
+    end = s.find_first_of(d);
+    while (end != string::npos) {
+      v[i++] = s.substr(start, end - start);
+      start = end + 1;
+      end = s.find_first_of(d, start);
+    }
+    v[i++] = s.substr(start, end - start);
+  } else {
+    int len = s.size();
+
+    v.resize(len);
+
+    for (i = 0; i < len; i++) {
+      v[i] = s[i];
+    }
+  }
 
   return v;
 }
 
 void split_to_container(const string& s, const string& d, vector<string>& v) {
-  size_t start = 0;
-  size_t end = s.find_first_of(d);
+  size_t start, end;
+
+  int count = 0;
+  start = 0;
+  end = s.find_first_of(d);
   while (end != string::npos) {
-    v.emplace_back(s.substr(start, end - start));
+    count++;
     start = end + 1;
     end = s.find_first_of(d, start);
   }
-  v.emplace_back(s.substr(start, s.size() - start));
+
+  int i;
+  if (count > 0) {
+    v.resize(count + 1);
+
+    i = 0;
+    start = 0;
+    end = s.find_first_of(d);
+    while (end != string::npos) {
+      v[i++] = s.substr(start, end - start);
+      start = end + 1;
+      end = s.find_first_of(d, start);
+    }
+    v[i++] = s.substr(start, end - start);
+  } else {
+    int len = s.size();
+
+    v.resize(len);
+
+    for (i = 0; i < len; i++) {
+      v[i] = s[i];
+    }
+  }
 }
 
 string inp_string(bool with_space = true, bool wants_dummy = true) {
@@ -64,7 +113,7 @@ void out_vector_string(vector<string>& v) {
 int main() {
   string s = inp_string();
 
-  vector<string> v = split(s, "");
+  vector<string> v = split(s, " ");
 
   out_vector_string(v);
 
