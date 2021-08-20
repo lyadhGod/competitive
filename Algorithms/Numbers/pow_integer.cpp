35bde2e79f111bcdf40c1f095a952deaf99846ef
Rounak Tikadar <rounak.tikadar@gmail.com>
2021-08-20 22:12:58 +0530

    changed mod_mul ref in pow_integer

--- Algorithms/Numbers/pow_integer.cpp
+++ Algorithms/Numbers/pow_integer.cpp
@@ -7,7 +7,6 @@ using namespace std;
 int pow_integer(
   const int& num,
   const unsigned int& exp,
-  const unsigned int& mod = 0,
   const bool& _debug = true
 ) {
   unsigned int a = exp;
@@ -17,50 +16,12 @@ int pow_integer(
   }
 
   int res = 1;
-  if (mod > 0) {
-    // From Numbers/mod.cpp
-    auto mod_mul = [&](
-      const int& a,
-      const int& b,
-      const unsigned int& mod
-    ) -> int {
-      int x = a;
-      if (x < 0) {
-        x += mod;
-      } else if (x >= mod) {
-        x %= mod;
-      }
-
-      int y = b;
-      if (y < 0) {
-        y += mod;
-      } else if (y >= mod) {
-        y %= mod;
-      }
-
-      int res = x * y;
-      if (res < 0) {
-        res += mod;
-      } else if (res >= mod) {
-        res %= mod;
-      }
-    };
-
-    while (a > 0) {
-      if (a & 1) {
-        res = mod_mul(res, n, mod);
-      }
-      n = mod_mul(n, n, mod);
-      a >>= 1;
+  while (a > 0) {
+    if (a & 1) {
+      res *= n;
     }
-  } else {
-    while (a > 0) {
-      if (a & 1) {
-        res *= n;
-      }
-      n *= n;
-      a >>= 1;
-    }
+    n *= n;
+    a >>= 1;
   }
 
   if ((num < 0) && (exp & 1)) {
@@ -75,11 +36,74 @@ int pow_integer(
   return res;
 }
 
+// Time: O(logN); Space: O(1);
+int pow_integer_mod(
+  const int& num,
+  const unsigned int& exp,
+  const unsigned int& mod,
+  const bool& _debug = true
+) {
+  unsigned int a = exp;
+  int n = num;
+  if (n < 0) {
+    n *= -1;
+  }
+
+  // From Numbers/mod.cpp#mod_mul
+  auto mod_mul = [&](
+    const int& a,
+    const int& b,
+    const unsigned int& mod
+  ) -> int {
+    int x = a;
+    if (x < 0) {
+      x += mod;
+    } else if (x >= mod) {
+      x %= mod;
+    }
+
+    int y = b;
+    if (y < 0) {
+      y += mod;
+    } else if (y >= mod) {
+      y %= mod;
+    }
+
+    int res = x * y;
+    if (res < 0) {
+      res += mod;
+    } else if (res >= mod) {
+      res %= mod;
+    }
+  };
+
+  int res = 1;
+  while (a > 0) {
+    if (a & 1) {
+      res = mod_mul(res, n, mod);
+    }
+    n = mod_mul(n, n, mod);
+    a >>= 1;
+  }
+
+  if ((num < 0) && (exp & 1)) {
+    res *= -1;
+  }
+
+  if (_debug) {
+    out_int(res, "pow_integer_mod()");
+    cout << "\n";
+  }
+
+  return res;
+}
+
 int main() {
   int num = inp_int("num", -10, 10);
   unsigned int exp_int = inp_int("exp", 0, 10);
 
-  pow_integer(num, exp_int, 1000000007);
+  pow_integer(num, exp_int);
+  pow_integer_mod(num, exp_int, 1000000007);
 
   return 0;
 }
